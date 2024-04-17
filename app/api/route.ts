924827dd37e18f8310d35df1d2c25c809aa0c9e3
Nick Vigier <nvigier46@gmail.com>
2024-04-17 10:20:06 +0100

    simple test

--- app/api/route.ts
+++ app/api/route.ts
@@ -7,7 +7,7 @@ const port = process.env.PORT;
 const API_URI = process.env.MONGODB_URI as string;
 const dbClient = new MongoClient(API_URI);
 
-const database = dbClient.db('rsvp-list');
+const database = dbClient.db('Cluster0');
 const invites = database.collection('invites');
 
 const retrieveRSVP = async (
@@ -50,16 +50,35 @@ const submitRSVP = async (
     return 'error';
   }
 };
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await dbClient.connect();
+    // Send a ping to confirm a successful connection
+    await dbClient.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } catch (e) {
+    console.error(e);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await dbClient.close();
+  }
+}
+
 export async function GET(request: NextRequest) {
   const rsvpCode = request.nextUrl.searchParams.get('rsvpCode');
 
   if (!rsvpCode) {
-    return new NextResponse('No code provided',  { status: 400 });
+    return new NextResponse('No code provided', { status: 400 });
   }
 
-  const rsvp = await retrieveRSVP(rsvpCode);
+  // const rsvp = await retrieveRSVP(rsvpCode);
+  await run();
 
-  return new Response(rsvp, {
+  return new Response('rsvp', {
     headers: {
       'content-type': 'application/json',
     },
